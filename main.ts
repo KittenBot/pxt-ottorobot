3232dca7669d926e62dedd9b6c23a58a3affc044
Riven <4771215@qq.com>
2018-11-07 18:48:44 +0800

    port movements

--- main.ts
+++ main.ts
@@ -19,6 +19,16 @@ namespace robototto {
         HL = 7
     }
 
+    export enum WalkDir {
+        Forward = 0,
+        Back = 1
+    }
+
+    export enum TurnDir {
+        Left = 0,
+        Right = 1
+    }
+
     let servoIndex: number[] = [0, 1, 2, 3, 4, 5, 6, 7];
     let _trim: number[] = [0, 0, 0, 0, 0, 0, 0, 0];
     let _degree: number[] = [90, 90, 90, 90, 90, 90, 90, 90];
@@ -55,10 +65,18 @@ namespace robototto {
         }
     }
 
+    function _moveServosLower(millis: number, m: number[]) {
+        _target[0] = m[0]
+        _target[1] = m[1]
+        _target[2] = m[2]
+        _target[3] = m[3]
+        _moveServos(millis);
+    }
+
     function setDegreePh(j: Joint, A: number, O: number, ph: number) {
         // use sin function to make servo sweep
-        let pos: number = Math.round(A*Math.sin(ph/180*Math.PI)+O)
-        setDegree(j, pos+90) // 90 degree = phase 0
+        let pos: number = Math.round(A * Math.sin(ph / 180 * Math.PI) + O)
+        setDegree(j, pos + 90) // 90 degree = phase 0
     }
 
     function _oscillateServos(period: number, cycle: number) {
@@ -68,7 +86,7 @@ namespace robototto {
         let totalSteps = period * cycle / step;
         let _inc = 360 * step / period; //phase increase for each 10ms
 
-        for (let s=0;s<totalSteps;s++){
+        for (let s = 0; s < totalSteps; s++) {
             for (let i = 0; i < 8; i++) {
                 setDegreePh(i, _amp[i], _offset[i], _ph[i]);
                 _ph[i] += _inc;
@@ -85,14 +103,6 @@ namespace robototto {
         setDegree(j, 90)
     }
 
-    //% blockId="ottoHome" block="Otto Home"
-    //% weight=100
-    export function ottoHome(): void {
-        for (let i = 0; i < 8; i++) {
-            setDegree(i, 90);
-        }
-    }
-
     function parseAry(str: string, ary: number[]) {
         let idx = 0;
         let lastC = 0;
@@ -105,9 +115,18 @@ namespace robototto {
         }
         // last number
         ary[idx] = parseInt(str.substr(lastC, str.length - lastC));
-        for (let i=0;i<8;i++){
-            serial.writeLine(i+" "+ ary[i])
+        for (let i = 0; i < 8; i++) {
+            serial.writeLine(i + " " + ary[i])
         }
+    }
+
+    //% blockId="ottoHome" block="Otto Home"
+    //% weight=100
+    export function ottoHome(): void {
+        for (let i = 0; i < 8; i++) {
+            _target[i] = 90;
+        }
+        _moveServos(1000);
     }
 
     /**
@@ -119,6 +138,143 @@ namespace robototto {
     export function moveServos(time: number, move: string): void {
         parseAry(move, _target);
         _moveServos(time);
+    }
+
+    //% blockId="ottoJump" block="Otto Jump |%time ms"
+    //% weight=80
+    export function ottoJump(time: number): void {
+        _moveServosLower(time, [90, 90, 150, 30]);
+        ottoHome();
+    }
+
+    //% blockId="ottoWalk" block="Otto Walk |%time ms|Dir %dir|Steps %step"
+    //% weight=80
+    export function ottoWalk(time: number, dir: WalkDir, step: number): void {
+        let tmp = (dir == WalkDir.Forward) ? -1 : 1;
+        _amp[0] = 30; _amp[1] = 30; _amp[2] = 20; _amp[3] = 20;
+        _offset[0] = 0; _offset[1] = 0; _offset[2] = 4; _offset[3] = -4;
+        _phase[0] = 0; _phase[1] = 0; _phase[2] = -90 * dir; _phase[3] = -90 * dir;
+        _oscillateServos(time, step);
+    }
+
+    //% blockId="ottoTurn" block="Otto Turn |%time ms|Dir %dir|Steps %step"
+    //% weight=80
+    export function ottoTurn(time: number, dir: TurnDir, step: number): void {
+        _amp[0] = 30; _amp[1] = 30; _amp[2] = 20; _amp[3] = 20;
+        _offset[0] = 0; _offset[1] = 0; _offset[2] = 4; _offset[3] = -4;
+        _phase[0] = 0; _phase[1] = 0; _phase[2] = -90 * dir; _phase[3] = -90 * dir;
+        if (dir == TurnDir.Left) {
+            _amp[0] = 30;
+            _amp[1] = 10;
+        } else {
+            _amp[0] = 10;
+            _amp[1] = 30;
+        }
+        _oscillateServos(time, step);
+    }
+
+    //% blockId="ottoBend" block="Otto Bend |%time ms|Dir %dir|Step %step"
+    //% weight=80
+    export function ottoBend(time: number, dir: number, step: number): void {
+        let bend1 = [90, 90, 62, 35];
+        let bend2 = [90, 90, 62, 105];
+        if (dir < 0) {
+            bend1[2] = 180 - 35;
+            bend1[3] = 180 - 65;
+            bend2[2] = 180 - 105;
+            bend2[3] = 180 - 60;
+        }
+        for (let j = 0; j < step; j++) {
+            _moveServosLower(400, bend1)
+            _moveServosLower(400, bend2)
+            basic.pause(time * 0.8)
+            ottoHome()
+        }
+    }
+
+    //% blockId="shakeLeg" block="Shake Leg |%time ms|Dir %dir|Steps %step"
+    //% weight=80
+    export function shakeLeg(T: number, dir: number, step: number): void {
+        let shake_leg1 = [90, 90, 58, 35];
+        let shake_leg2 = [90, 90, 58, 120];
+        let shake_leg3 = [90, 90, 58, 60];
+        if (dir<0){
+            shake_leg1[2]=180-35;
+            shake_leg1[3]=180-58;
+            shake_leg2[2]=180-120;
+            shake_leg2[3]=180-58;
+            shake_leg3[2]=180-60;
+            shake_leg3[3]=180-58;
+        }
+        let numberLegMoves = 2;
+        //Time of the bend movement. Fixed parameter to avoid falls
+        let T2= 1000;
+        //Time of one shake, constrained in order to avoid movements too fast.            
+        T = T - T2;
+        T = Math.max(T, 200 * numberLegMoves);  
+        for (let j= 0; j < step;j++)
+        {
+            //Bend movement
+            _moveServosLower(T2 / 2, shake_leg1);
+            _moveServosLower(T2 / 2, shake_leg2);
+
+            //Shake movement
+            for (let i= 0; i < numberLegMoves;i++)
+            {
+                _moveServosLower(T / (2 * numberLegMoves), shake_leg3);
+                _moveServosLower(T / (2 * numberLegMoves), shake_leg2);
+            }
+            ottoHome()
+        }
+        basic.pause(T)
+    }
+
+    //% blockId="ottoUpdown" block="Otto Updown |%time ms|Height %h|Steps %step"
+    //% weight=80
+    export function ottoUpdown(time: number, h: number, step: number): void {
+        _amp[0] = 0; _amp[1] = 0; _amp[2] = h; _amp[3] = h;
+        _offset[0] = 0; _offset[1] = 0; _offset[2] = h; _offset[3] = -h;
+        _phase[0] = 0; _phase[1] = 0; _phase[2] = -90; _phase[3] = 90;
+        _oscillateServos(time, step);
+    }
+
+    //% blockId="ottoSwing" block="Otto Swing |%time ms|Swing %h|Steps %step"
+    //% weight=80
+    export function ottoSwing(time: number, h: number, step: number): void {
+        _amp[0] = 0; _amp[1] = 0; _amp[2] = h; _amp[3] = h;
+        _offset[0] = 0; _offset[1] = 0; _offset[2] = h/2; _offset[3] = -h/2;
+        _phase[0] = 0; _phase[1] = 0; _phase[2] = 0; _phase[3] = 0;
+        _oscillateServos(time, step);
+    }
+
+    //% blockId="ottoJitter" block="Otto Jitter |%time ms|Height %h|Steps %step"
+    //% weight=80
+    export function ottoJitter(time: number, h: number, step: number): void {
+        h = Math.min(25,h);
+        _amp[0] = h; _amp[1] = h; _amp[2] = 0; _amp[3] = 0;
+        _offset[0] = 0; _offset[1] = 0; _offset[2] = 0; _offset[3] = 0;
+        _phase[0] = -90; _phase[1] = 90; _phase[2] = 0; _phase[3] = 0;
+        _oscillateServos(time, step);
+    }
+
+    //% blockId="ottoMoonWalk" block="Otto Moon Walk |%time ms|Height %h|Steps %step"
+    //% weight=80
+    export function ottoMoonWalk(time: number, h: number, step: number): void {
+        _amp[0] = h; _amp[1] = h; _amp[2] = h; _amp[3] = h;
+        _offset[0] = 0; _offset[1] = 0; _offset[2] = h / 2 + 2; _offset[3] = -h / 2 - 2;
+        let phi = -90;
+        _phase[0] = 0; _phase[1] = 0; _phase[2] = phi; _phase[3] = -60+phi;
+        _oscillateServos(time, step);
+    }
+
+    //% blockId="ottoFlapping" block="Otto Flapping |%time ms|Height %h|Steps %step"
+    //% weight=80
+    export function ottoFlapping(time: number, h: number, step: number): void {
+        let dir = 1;
+        _amp[0] = 12; _amp[1] = 12; _amp[2] = h; _amp[3] = h;
+        _offset[0] = 0; _offset[1] = 0; _offset[2] = h -10; _offset[3] = -h +10;
+        _phase[0] = 0; _phase[1] = 180; _phase[2] = -90*dir; _phase[3] =90*dir;
+        _oscillateServos(time, step);
     }
 
     /**
