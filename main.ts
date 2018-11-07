72bc4f0119469c2c641d5c5dc7d568605d52f48a
Riven <4771215@qq.com>
2018-11-07 16:13:13 +0800

    add basic servo movings

--- main.ts
+++ main.ts
@@ -20,18 +20,48 @@ namespace robototto {
     }
 
     let servoIndex: number[] = [0, 1, 2, 3, 4, 5, 6, 7];
-    let trim: number[] = [0, 0, 0, 0, 0, 0, 0, 0];
-    let movement: number[] = [0,0,0,0,0,0,0,0];
+    let _trim: number[] = [0, 0, 0, 0, 0, 0, 0, 0];
+    let _degree: number[] = [90, 90, 90, 90, 90, 90, 90, 90];
+    let _target: number[] = [90, 90, 90, 90, 90, 90, 90, 90];
+    let _increment: number[] = [0, 0, 0, 0, 0, 0, 0, 0];
+    let _amp: number[] = [0, 0, 0, 0, 0, 0, 0, 0];
+    
 
     function setDegree(j: Joint, degree: number) {
-        robotbit.Servo(servoIndex[j], degree + trim[j])
+        robotbit.Servo(servoIndex[j], degree + _trim[j])
+        _degree[j] = degree;
+        serial.writeLine(j + " " + degree)
+    }
+
+
+    function _moveServos(millis: number) {
+        let steps: number;
+        if (millis>10){
+            steps = millis/10;
+            for (let i = 0; i < 8; i++) {
+                _increment[i] = (_target[i] - _degree[i])/steps;
+            }
+            for (let s=0;s<steps;s++){
+                for (let i = 0; i < 8; i++) {
+                    setDegree(i, _degree[i] + _increment[i])
+                }
+                basic.pause(10);
+            }
+        }
+        for (let i = 0; i < 8; i++) {
+            setDegree(i, _target[i])
+        }
+    }
+
+    function _oscillateServos() {
+        
     }
 
     //% blockId="ottoAttach" block="Attach |Joint %j|%s|Trim %t"
     //% weight=100
     export function ottoAttach(j: Joint, s: robotbit.Servos, t: number): void {
         servoIndex[j] = s;
-        trim[j] = t;
+        _trim[j] = t;
         setDegree(j, 90)
     }
 
@@ -53,14 +83,15 @@ namespace robototto {
         let idx = 0;
         let lastC = 0;
         for (let i=0;i<move.length;i++){
-            if (move.charAt(i) == ' ' || move.charAt(i) == '\0'){
-                movement[idx] = parseInt(move.substr(lastC, i-lastC));
+            if (move.charAt(i) == ' ' || move.charAt(i) == ','){
+                _target[idx] = parseInt(move.substr(lastC, i-lastC));
                 lastC = i;
                 idx++;
             }
         }
-        movement[idx] = parseInt(move.substr(lastC, move.length - lastC));
-
+        // last number
+        _target[idx] = parseInt(move.substr(lastC, move.length - lastC));
+        _moveServos(time);
     }
 
 
